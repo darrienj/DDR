a7a4c219f07a8b9a71ee56bf500423e9dc9e812c
drichmond <daBlackSaiyan27@gmail.com>
2016-10-23 01:14:32 -0500

    swap rxtx library for nrjavaserial library

--- src/display/DancePanelInput.java
+++ src/display/DancePanelInput.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
-import gnu.io.CommPortIdentifier; 
+import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent; 
 import gnu.io.SerialPortEventListener; 
